035a7c15d4c398d97e66b482bd7f673d3a89258b
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-12-15 00:04:11 +0530

    verify email
    verify email

--- src/Components/Pages/Welcome/Welcome.js
+++ src/Components/Pages/Welcome/Welcome.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import LoginContext from "../../Context/LoginContext";
 
 const Welcome = () => {
+  const loginCtx = useContext(LoginContext);
+  const verifyEmailHandler = async () => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBkbQXz7lJKNU-7T6SEpXkPqb22k2yINFg",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: loginCtx.idToken,
+          requestType: "VERIFY_EMAIL",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.ok) {
+      console.log(data.email);
+    } else {
+      alert(data.error.message);
+    }
+  };
   return (
     <div>
       <h2>Welcome To Expense Tracker</h2>
